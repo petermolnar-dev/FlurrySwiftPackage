8e853dcf84e13baf923580ebead00b41be209da3
Peter Molnar <peter.molnar@alludo.com>
2023-01-04 12:52:57 +0100

    Removal CrashReporter as a library

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Flurry",
-            targets: ["Flurry","CrashReporter"]
+            targets: ["Flurry"]
         ),
         .library(
             name: "FlurryMessaging",
